4d5720d65f009712fe5a22cbb8aa8028332adb0b
Dubz <Dubz@users.noreply.github.com>
2019-09-15 16:07:44 -0400

    Add better support for running on servers and added support for running locally

--- update_ssl_controller.sh
+++ update_ssl_controller.sh
@@ -84,7 +84,11 @@ else
     # Did the keys change? If not, no sense in continuing...
     if [ "${sha512_privkey}" == "${sha512_last}" ]; then
         # Did it change there? If no, no sense in continuing...
-        sha512_controller=$(sshpass -p "${CONTROLLER_PASS}" ssh -o LogLevel=error ${CONTROLLER_USER}@${CONTROLLER_HOST} "openssl rsa -noout -modulus -in \"/etc/ssl/private/cloudkey.key\" | openssl sha512")
+        if [ "${CONTROLLER_LOCAL}" == "true"]; then
+            sha512_controller=$(openssl rsa -noout -modulus -in "/etc/ssl/private/cloudkey.key" | openssl sha512)
+        else
+            sha512_controller=$(sshpass -p "${CONTROLLER_PASS}" ssh -o LogLevel=error ${CONTROLLER_USER}@${CONTROLLER_HOST} "openssl rsa -noout -modulus -in \"/etc/ssl/private/cloudkey.key\" | openssl sha512")
+        fi
         if [ "${sha512_privkey}" != "${sha512_controller}" ]; then
             echo "Key is not on controller, installer will continue!"
         else
@@ -115,45 +119,92 @@ echo "done!"
 
 # Backup original keystore on CK
 echo -n "Creating backup of keystore on controller..."
-# sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "if [ -s \"${CONTROLLER_KEYSTORE}.orig\" ]; then cp -n \"${CONTROLLER_KEYSTORE}\" \"${CONTROLLER_KEYSTORE}.orig\"; else cp -n \"${CONTROLLER_KEYSTORE}\" \"${CONTROLLER_KEYSTORE}.bak\"; fi"
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} 'if [ -s "'${CONTROLLER_KEYSTORE}'.orig" ]; then echo -n "Backup of original keystore exists! Creating non-destructive backup as keystore.bak..."; sudo cp -n "'${CONTROLLER_KEYSTORE}'" "'${CONTROLLER_KEYSTORE}'.bak"; else echo -n "no original keystore backup found. Creating backup as keystore.orig..."; sudo cp -n "'${CONTROLLER_KEYSTORE}'" "'${CONTROLLER_KEYSTORE}'.orig"; fi'
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    if [ -s "'${CONTROLLER_KEYSTORE}'.orig" ]; then
+        echo -n "Backup of original keystore exists! Creating non-destructive backup as keystore.bak...";
+        sudo cp -n "'${CONTROLLER_KEYSTORE}'" "'${CONTROLLER_KEYSTORE}'.bak";
+    else
+        echo -n "no original keystore backup found. Creating backup as keystore.orig...";
+        sudo cp -n "'${CONTROLLER_KEYSTORE}'" "'${CONTROLLER_KEYSTORE}'.orig";
+    fi
+else
+    # sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "if [ -s \"${CONTROLLER_KEYSTORE}.orig\" ]; then cp -n \"${CONTROLLER_KEYSTORE}\" \"${CONTROLLER_KEYSTORE}.orig\"; else cp -n \"${CONTROLLER_KEYSTORE}\" \"${CONTROLLER_KEYSTORE}.bak\"; fi"
+    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} 'if [ -s "'${CONTROLLER_KEYSTORE}'.orig" ]; then echo -n "Backup of original keystore exists! Creating non-destructive backup as keystore.bak..."; sudo cp -n "'${CONTROLLER_KEYSTORE}'" "'${CONTROLLER_KEYSTORE}'.bak"; else echo -n "no original keystore backup found. Creating backup as keystore.orig..."; sudo cp -n "'${CONTROLLER_KEYSTORE}'" "'${CONTROLLER_KEYSTORE}'.orig"; fi'
+fi
 echo "done!"
 
-# Backup original keys on CK
-echo -n "Creating backups of cloudkey.key and cloudkey.crt on controller..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} 'for f in {cloudkey.key,cloudkey.crt}; do sudo cp -n "/etc/ssl/private/${f}" "/etc/ssl/private/${f}.bak"; done'
-echo "done!"
+# Backup original keys on CK for nginx
+if [ "${CONTROLLER_IS_CK}" == "true"]; then
+    echo -n "Creating backups of cloudkey.key and cloudkey.crt on controller..."
+    if [ "${CONTROLLER_LOCAL}" == "true"]; then
+        for f in {cloudkey.key,cloudkey.crt}; do
+            sudo cp -n "/etc/ssl/private/${f}" "/etc/ssl/private/${f}.bak";
+        done
+    else
+        sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} 'for f in {cloudkey.key,cloudkey.crt}; do sudo cp -n "/etc/ssl/private/${f}" "/etc/ssl/private/${f}.bak"; done'
+    fi
+    echo "done!"
+fi
 
 
 # Copy over...
 
 # Copy to CK
 echo -n "Copying files to controller..."
-sshpass -p "${CONTROLLER_PASS}" scp -q "${CERTBOT_LOCAL_DIR_CONFIG}/live/${CONTROLLER_HOST}/fullchain.pem" ${CONTROLLER_USER}@${CONTROLLER_HOST}:"/etc/ssl/private/cloudkey.crt"
-sshpass -p "${CONTROLLER_PASS}" scp -q "${CERTBOT_LOCAL_DIR_CONFIG}/live/${CONTROLLER_HOST}/privkey.pem" ${CONTROLLER_USER}@${CONTROLLER_HOST}:"/etc/ssl/private/cloudkey.key"
-sshpass -p "${CONTROLLER_PASS}" scp -q "${CERTBOT_LOCAL_DIR_CACHE}/${CONTROLLER_HOST}/fullchain.p12" ${CONTROLLER_USER}@${CONTROLLER_HOST}:"${CONTROLLER_JAVA_DIR}/data/fullchain.p12"
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    if [ "${CONTROLLER_IS_CK}" == "true"]; then
+        cp "${CERTBOT_LOCAL_DIR_CONFIG}/live/${CONTROLLER_HOST}/fullchain.pem" "/etc/ssl/private/cloudkey.crt"
+        cp "${CERTBOT_LOCAL_DIR_CONFIG}/live/${CONTROLLER_HOST}/privkey.pem" "/etc/ssl/private/cloudkey.key"
+    fi
+    cp "${CERTBOT_LOCAL_DIR_CACHE}/${CONTROLLER_HOST}/fullchain.p12" $"${CONTROLLER_JAVA_DIR}/data/fullchain.p12"
+else
+    if [ "${CONTROLLER_IS_CK}" == "true"]; then
+        sshpass -p "${CONTROLLER_PASS}" scp -q "${CERTBOT_LOCAL_DIR_CONFIG}/live/${CONTROLLER_HOST}/fullchain.pem" ${CONTROLLER_USER}@${CONTROLLER_HOST}:"/etc/ssl/private/cloudkey.crt"
+        sshpass -p "${CONTROLLER_PASS}" scp -q "${CERTBOT_LOCAL_DIR_CONFIG}/live/${CONTROLLER_HOST}/privkey.pem" ${CONTROLLER_USER}@${CONTROLLER_HOST}:"/etc/ssl/private/cloudkey.key"
+    fi
+    sshpass -p "${CONTROLLER_PASS}" scp -q "${CERTBOT_LOCAL_DIR_CACHE}/${CONTROLLER_HOST}/fullchain.p12" ${CONTROLLER_USER}@${CONTROLLER_HOST}:"${CONTROLLER_JAVA_DIR}/data/fullchain.p12"
+fi
 echo "done!"
 
 
 # Stop service...
 echo -n "Stopping UniFi Controller..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service ${CONTROLLER_SERVICE_UNIFI_NETWORK} stop"
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    service ${CONTROLLER_SERVICE_UNIFI_NETWORK} stop
+else
+    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service ${CONTROLLER_SERVICE_UNIFI_NETWORK} stop"
+fi
 echo "done!"
 
 
 # Load keystore changes
 echo -n "Removing previous certificate data from UniFi keystore..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "keytool -delete -alias ${CONTROLLER_KEYSTORE_ALIAS} -keystore ${CONTROLLER_KEYSTORE} -deststorepass ${CONTROLLER_KEYSTORE_PASSWORD}"
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    keytool -delete -alias ${CONTROLLER_KEYSTORE_ALIAS} -keystore ${CONTROLLER_KEYSTORE} -deststorepass ${CONTROLLER_KEYSTORE_PASSWORD}
+else
+    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "keytool -delete -alias ${CONTROLLER_KEYSTORE_ALIAS} -keystore ${CONTROLLER_KEYSTORE} -deststorepass ${CONTROLLER_KEYSTORE_PASSWORD}"
+fi
 echo "done!"
 echo -n "Importing SSL certificate into UniFi keystore..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "keytool -importkeystore \
-    -srckeystore \"${CONTROLLER_JAVA_DIR}/data/fullchain.p12\" \
-    -srcstoretype PKCS12 \
-    -srcstorepass ${CONTROLLER_KEYSTORE_PASSWORD} \
-    -destkeystore ${CONTROLLER_KEYSTORE} \
-    -deststorepass ${CONTROLLER_KEYSTORE_PASSWORD} \
-    -destkeypass ${CONTROLLER_KEYSTORE_PASSWORD} \
-    -alias ${CONTROLLER_KEYSTORE_ALIAS}"
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    keytool -importkeystore \
+        -srckeystore \"${CONTROLLER_JAVA_DIR}/data/fullchain.p12\" \
+        -srcstoretype PKCS12 \
+        -srcstorepass ${CONTROLLER_KEYSTORE_PASSWORD} \
+        -destkeystore ${CONTROLLER_KEYSTORE} \
+        -deststorepass ${CONTROLLER_KEYSTORE_PASSWORD} \
+        -destkeypass ${CONTROLLER_KEYSTORE_PASSWORD} \
+        -alias ${CONTROLLER_KEYSTORE_ALIAS}
+else
+    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "keytool -importkeystore \
+        -srckeystore \"${CONTROLLER_JAVA_DIR}/data/fullchain.p12\" \
+        -srcstoretype PKCS12 \
+        -srcstorepass ${CONTROLLER_KEYSTORE_PASSWORD} \
+        -destkeystore ${CONTROLLER_KEYSTORE} \
+        -deststorepass ${CONTROLLER_KEYSTORE_PASSWORD} \
+        -destkeypass ${CONTROLLER_KEYSTORE_PASSWORD} \
+        -alias ${CONTROLLER_KEYSTORE_ALIAS}"
+fi
 echo "done!"
 
 
@@ -163,24 +214,42 @@ echo "done!"
 # echo "done!"
 # Start service back up
 echo -n "Restarting UniFi Controller to apply new Let's Encrypt SSL certificate..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service ${CONTROLLER_SERVICE_UNIFI_NETWORK} start"
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    service ${CONTROLLER_SERVICE_UNIFI_NETWORK} start
+else
+    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service ${CONTROLLER_SERVICE_UNIFI_NETWORK} start"
+fi
 echo "done!"
 
 # Reload nginx on the CloudKey
 echo -n "Reloading nginx..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service nginx reload"
+if [ "${CONTROLLER_IS_CK}" == "true"]; then
+    if [ "${CONTROLLER_LOCAL}" == "true"]; then
+        service nginx reload
+    else
+        sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service nginx reload"
+    fi
+fi
 echo "done!"
 
 if [ "${CONTROLLER_HAS_PROTECT}" == "true" ]; then
     # Reload Protect On the CloudKey
     echo -n "Reloading UniFi Protect..."
-    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service ${CONTROLLER_SERVICE_UNIFI_PROTECT} reload"
+    if [ "${CONTROLLER_LOCAL}" == "true"]; then
+        service ${CONTROLLER_SERVICE_UNIFI_PROTECT} reload
+    else
+        sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "service ${CONTROLLER_SERVICE_UNIFI_PROTECT} reload"
+    fi
     echo "done!"
 fi
 
 
 echo -n "Cleaning up CloudKey..."
-sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "rm -f \"${CONTROLLER_JAVA_DIR}/data/fullchain.p12\""
+if [ "${CONTROLLER_LOCAL}" == "true"]; then
+    rm -f "${CONTROLLER_JAVA_DIR}/data/fullchain.p12"
+else
+    sshpass -p "${CONTROLLER_PASS}" ssh ${CONTROLLER_USER}@${CONTROLLER_HOST} "rm -f \"${CONTROLLER_JAVA_DIR}/data/fullchain.p12\""
+fi
 echo "done!"
 
 
